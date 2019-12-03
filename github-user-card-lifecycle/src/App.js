c1de69a42f384ad0f5ccb252d26d844026759ed5
AbdelIdir <homeworkoracle@protonmail.com>
2019-12-03 15:52:31 +0100

    created my github card using react bootstrap and class componenets

--- github-user-card-lifecycle/src/App.js
+++ github-user-card-lifecycle/src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      users: {}
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/AbdelIdir")
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+          users: response.data
+        });
+      })
+
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={this.state.users.avatar_url} />
+          <Card.Body>
+            <Card.Title>{this.state.users.name}</Card.Title>
+            <Card.Text>Bio: {this.state.users.bio}</Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>Location: {this.state.users.location}</ListGroupItem>
+            <ListGroupItem>
+              Created: {this.state.users.created_at}
+            </ListGroupItem>
+            <ListGroupItem>
+              Followers: {this.state.users.followers}
+            </ListGroupItem>
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href={this.state.users.html_url}>Card Link</Card.Link>
+            <Card.Link href={this.state.users.organizations_url}>
+              Organizations
+            </Card.Link>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default App;
